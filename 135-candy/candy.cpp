b1b81eaad7e424560ffb4838d36e12e861cd116b
Rajat Singh <163058419+Therajat14@users.noreply.github.com>
2026-08-16 14:27:47 +0530

    Time: 4 ms (15.78%) | Memory: 73.5 MB (5.05%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -9,19 +9,22 @@ public:
         for(int i = 0; i < n; i++){
             if(i - 1 >=0 && ratings[i - 1] < ratings[i])
                 ltr[i] += ltr[i - 1];
-
         }
+                
+        int candy = 0;
+        int right = 1;
 
         for(int i = n - 1; i >= 0; i--){
             if(i + 1 < n && ratings[i + 1] < ratings[i]){
-                rtl[i] += rtl[i + 1];
+                right = right + 1;
             }
+            else right = 1;
+
+
+            candy += max(ltr[i], right);
         }
 
-        int candy = 0;
-        for(int i = 0; i < n; i++){
-            candy += max(rtl[i], ltr[i]);
-        }
+
 
         return candy;
     }
